11bb1b58d4a351d0e58554298c165c88c8a6be02
Ice Nattanon <153441912+icenattanon@users.noreply.github.com>
2023-12-24 23:23:22 +0000

    Add auth and signin page

--- code-first-app/src/app/page.tsx
+++ code-first-app/src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 w-1/2 m-auto">
+      <h1>Hollo World</h1>
     </main>
   )
 }
